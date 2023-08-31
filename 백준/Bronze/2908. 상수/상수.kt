d0e4f8ce893ac59033be417dc651ebf915580256
Shin JungHoon <42196666+junghoonshin3@users.noreply.github.com>
2023-08-31 16:10:02 +0900

    [Bronze II] Title: 상수, Time: 128 ms, Memory: 20704 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.kt
+++ 백준/Bronze/2908. 상수/상수.kt
@@ -1,34 +1,8 @@
-import java.util.Stack
-
-
 fun main() {
     val reader = System.`in`.bufferedReader()
     val input = reader.readLine().split(" ")
-    val num1 = input.first()
-    val num2 = input.last()
-    val stack1 = Stack<String>()
-    val stack2 = Stack<String>()
-    var reverse1 = ""
-    var reverse2 = ""
-    num1.forEach {
-        stack1.add(it.toString())
-    }
-    num2.forEach {
-        stack2.add(it.toString())
-    }
-    while (!stack1.isEmpty()) {
-        reverse1 += stack1.pop()
-    }
+    val num1 = input.first().reversed()
+    val num2 = input.last().reversed()
 
-    while (!stack2.isEmpty()) {
-        reverse2 += stack2.pop()
-    }
-
-    val r = if (reverse1 > reverse2) {
-        reverse1
-    } else {
-        reverse2
-    }
-
-    println(r)
+    println(if (num1 > num2) num1 else num2)
 }
