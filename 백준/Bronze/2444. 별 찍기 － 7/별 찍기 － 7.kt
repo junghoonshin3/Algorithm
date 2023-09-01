b83c24414b735daf47d6abc2b5bda990ef030188
Shin JungHoon <42196666+junghoonshin3@users.noreply.github.com>
2023-09-01 13:11:59 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 384 ms, Memory: 16636 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.kt
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.kt
@@ -1,14 +1,16 @@
 fun main() {
     val reader = System.`in`.bufferedReader()
+
     val n = reader.readLine().toInt()
+
     for (i in 1..n) {
         for (j in 1..n - i) print(" ")
-        for (j in 1..(2 * i) - 1) print("*")
+        for (j in 1..2 * i - 1) print("*")
         println()
     }
-    for (i in n - 1 downTo (1)) {
-        for (j in i until n) print(" ")
-        for (j in 1 until 2 * i) print("*")
+    for (i in 1..n) {
+        for (j in 1..i) print(" ")
+        for (j in i until (2 * n - 1) - i) print("*")
         println()
     }
 }
