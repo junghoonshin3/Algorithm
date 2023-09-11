1061dc03e3bb5de9c198ada079620054ea189ff9
Shin JungHoon <42196666+junghoonshin3@users.noreply.github.com>
2023-09-11 16:11:20 +0900

    [Silver IV] Title: 설탕 배달, Time: 100 ms, Memory: 12292 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.kt
@@ -0,0 +1,20 @@
+fun main() {
+
+    var n = readLine()!!.toInt()
+    var result = 0
+
+    while (n > 0) {
+        if (n % 5 == 0) {
+            result += n / 5
+            n %= 5
+            break
+        }
+        n -= 3
+        result += 1
+    }
+    if (n != 0) {
+        result = -1
+    }
+    println(result)
+
+}
