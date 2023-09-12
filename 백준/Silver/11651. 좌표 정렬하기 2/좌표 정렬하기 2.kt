6d0a4ead790c024531d2ab1f0c9f05488390f1e6
Shin JungHoon <42196666+junghoonshin3@users.noreply.github.com>
2023-09-12 17:32:58 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 1916 ms, Memory: 88364 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.kt
@@ -0,0 +1,16 @@
+fun main() {
+    val reader = System.`in`.bufferedReader()
+    val n = reader.readLine().toInt()
+    val array = Array(n) { Pair(0, 0) }
+    repeat(n) {
+        val s = reader.readLine().split(" ").map { it.toInt() }
+        array[it] = Pair(s[0], s[1])
+    }
+
+    array.sortWith(compareBy<Pair<Int, Int>> {
+        it.second
+    }.thenBy { it.first })
+    array.forEach {
+        println("${it.first} ${it.second}")
+    }
+}
