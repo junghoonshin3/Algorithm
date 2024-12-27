300d7028d524c9953ed6af9588d565631343c517
Shin JungHoon <42196666+junghoonshin3@users.noreply.github.com>
2024-12-27 19:28:44 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.09 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int solution(int[] bandage, int health, int[][] attacks) {
+
+    
+        int life = health;
+        int successCount = 0; 
+        int attackIndex = 0;
+        
+        for(int i = 1; i <= attacks[attacks.length-1][0]; i++){
+            if(life <= 0){
+                break;
+            }
+            if(i != attacks[attackIndex][0]){
+                life += bandage[1];
+                successCount ++;
+                    if(successCount == bandage[0]){
+                        life += bandage[2];
+                        successCount= 0;
+                    }
+                    if(life > health){
+                        life = health;
+                    }
+            }else{
+                 life -= attacks[attackIndex][1];
+                 successCount = 0;
+                 attackIndex++;
+            }
+        }
+            
+        return life <= 0 ? -1 : life;
+    }
+}
