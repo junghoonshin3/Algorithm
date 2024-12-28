e439f87c06d230c15983d88fe53f84194560947a
Shin JungHoon <42196666+junghoonshin3@users.noreply.github.com>
2024-12-28 16:23:23 +0900

    [level 0] Title: 5명씩, Time: 0.02 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/0/181886. 5명씩/5명씩.java
+++ 프로그래머스/0/181886. 5명씩/5명씩.java
@@ -1,11 +1,9 @@
 class Solution {
     public String[] solution(String[] names) {
-        String answer = "";
-        for(int i =0; i<names.length; i++){
-            if(i % 5 == 0){
-                answer += names[i]+",";
-            }
-        }
-        return answer.split(",");
+       int idx = 0;
+        String[] answer = new String[names.length % 5 == 0 ? names.length / 5 : names.length / 5 + 1];
+        for (int i = 0;i < names.length;i+=5)
+            answer[idx++] = names[i];
+        return answer;
     }
 }
