d780b2dabd19955df5ae5900380ebb344bab8e6c
Shin JungHoon <42196666+junghoonshin3@users.noreply.github.com>
2024-12-30 18:51:56 +0900

    [level 0] Title: 안전지대, Time: 0.04 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/0/120866. 안전지대/안전지대.java
+++ 프로그래머스/0/120866. 안전지대/안전지대.java
@@ -1,25 +1,32 @@
 class Solution {
     public int solution(int[][] board) {
         int answer = 0;
-        int[][] result = new int[board.length + 2][board.length + 2];
-            
-        int[][] list = new int[board.length+2][board.length+2];
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board.length;j++){
-                if(board[i][j] == 1){
-                    for(int a=i;a<=i+2;a++){
-                        for(int b=j;b<=j+2;b++){
-                            if(list[a][b]!=1)
-                                list[a][b] = 2;
+        int[][] newBoard = new int[board.length + 2][board.length + 2];
+        
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board.length; j++){
+                newBoard[i+1][j+1] = board[i][j];
+            }
+        }
+        
+        for(int i = 1; i < newBoard.length; i++){
+            for(int j = 1; j < newBoard.length; j++){
+                if(newBoard[i][j] == 1){
+                    for(int k = i - 1; k <= i + 1; k++){
+                        for(int m = j - 1; m <= j + 1; m++){
+                            if(newBoard[k][m]!=1){
+                                newBoard[k][m] = 2;
+                            }
                         }
                     }
                 }
             }
         }
-        for(int i = 1;i<list.length-1;i++){
-            for(int j = 1;j<list.length-1;j++){
-                if(list[i][j] == 0){
-                    answer++;
+        
+        for(int i = 1; i < newBoard.length-1; i++){
+            for(int j = 1; j < newBoard.length-1; j++){
+                if(newBoard[i][j] == 0){
+                    answer ++;
                 }
             }
         }
